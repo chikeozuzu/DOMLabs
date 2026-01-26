f195ce7362d1724b67a05ebabfcf8e9f5a6faf16
chikeozuzu <chikeozuzu@gmail.com>
2026-01-26 02:05:17 -0500

    Began working on the Part 1 of the lab in the JS file

--- ALAB316_1/index.js
+++ ALAB316_1/index.js
@@ -1,1 +1,7 @@
 import "./styles.css";
+
+/*  DOM manipulation focusing on adding static elements created with JavaScript without modifying
+    any of the content of the index.html or styles.css files.
+*/
+
+//  Part 1
