a03ec602362e38727da59195f452d61952fc9617
chikeozuzu <chikeozuzu@gmail.com>
2026-01-28 19:27:25 -0500

    Completed working on the Part 1 of the lab in the JS file

--- ALAB316_1/index.js
+++ ALAB316_1/index.js
@@ -5,3 +5,18 @@ import "./styles.css";
 */
 
 //  Part 1
+
+/*  Start the project by building a main content element using the following steps:
+    Select and cache the <main> element in a variable named mainEl.
+    Set the background color of mainEl to the value stored in the --main-bg CSS custom property.
+        Hint: Assign a string that uses the CSS var() function like this: 'var(--main-bg)'.
+    Set the content of mainEl to <h1>DOM Manipulation</h1>.
+    Add a class of flex-ctr to mainEl.
+        Hint: Use the Element.classList API.
+*/
+
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+mainEl.classList.add("flex-ctr");
+
