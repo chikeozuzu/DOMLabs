bb0fa99ff64968a60664e97f5a068f7195ff31b9
chikeozuzu <chikeozuzu@gmail.com>
2026-01-28 19:39:10 -0500

    Began working on the Part 3 of the lab in the JS file

--- ALAB316_1/index.js
+++ ALAB316_1/index.js
@@ -42,3 +42,13 @@ topMenuEl.style.height = "100%";
 topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 topMenuEl.classList.add("flex-around");
 
+//  Part 3
+
+/*  Now that the menu bar is styled, we can add some menu links to it.
+    Iterate over the menuLinks array and for each "link" object:
+    Create an <a> element.
+    On the new element, add an href attribute with its value set to the href property of the "link" object.
+    Set the new element's content to the value of the text property of the "link" object.
+    Append the new element to the topMenuEl element.
+*/
+
