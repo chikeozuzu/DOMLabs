9352195668b259ddd8e03f85383fbbff62c52650
chikeozuzu <chikeozuzu@gmail.com>
2026-01-28 19:32:47 -0500

    Completed working on the Part 2 of the lab in the JS file

--- ALAB316_1/index.js
+++ ALAB316_1/index.js
@@ -22,4 +22,15 @@ mainEl.classList.add("flex-ctr");
 
 //  Part 2
 
-/*
+/*  Next, create a blank menu bar that we can use to later add some interactivity to the page:
+    Select and cache the <nav id="top-menu"> element in a variable named topMenuEl.
+    Set the height of the topMenuEl element to be 100%.
+    Set the background color of topMenuEl to the value stored in the --top-menu-bg CSS custom property.
+    Add a class of flex-around to topMenuEl.
+*/
+
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
