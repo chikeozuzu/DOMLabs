4cccd655f5c0cb1917207270cdecdbee35d9cfdd
chikeozuzu <chikeozuzu@gmail.com>
2026-02-01 21:26:40 -0500

    Updated the menulinks array in the JavaScript file as instructed for the RALAB316.3 Lab

--- RALAB316_3/index.js
+++ RALAB316_3/index.js
@@ -1,27 +1,44 @@
 // Menu data structure
 var menuLinks = [
-  { text: 'about', href: '/about' },
-  { text: 'catalog', href: '/catalog' },
-  { text: 'orders', href: '/orders' },
-  { text: 'account', href: '/account' },
+    { text: 'about', href: '/about' },
+    {
+        text: 'catalog', href: '#', subLinks: [
+            { text: 'all', href: '/catalog/all' },
+            { text: 'top selling', href: '/catalog/top' },
+            { text: 'search', href: '/catalog/search' },
+        ]
+    },
+    {
+        text: 'orders', href: '#', subLinks: [
+            { text: 'new', href: '/orders/new' },
+            { text: 'pending', href: '/orders/pending' },
+            { text: 'history', href: '/orders/history' },
+        ]
+    },
+    {
+        text: 'account', href: '#', subLinks: [
+            { text: 'profile', href: '/account/profile' },
+            { text: 'sign out', href: '/account/signout' },
+        ]
+    },
 ];
 
 document.addEventListener('DOMContentLoaded', function () {
-  const mainEl = document.querySelector('main');
-  mainEl.style.backgroundColor = 'var(--main-bg)';
-  mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
-  mainEl.classList.add('flex-ctr');
+    const mainEl = document.querySelector('main');
+    mainEl.style.backgroundColor = 'var(--main-bg)';
+    mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
+    mainEl.classList.add('flex-ctr');
 
-  const topMenuEl = document.getElementById('top-menu');
-  topMenuEl.style.height = '100%';
-  topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
-  topMenuEl.classList.add('flex-around');
+    const topMenuEl = document.getElementById('top-menu');
+    topMenuEl.style.height = '100%';
+    topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+    topMenuEl.classList.add('flex-around');
 
-  menuLinks.forEach(function (link) {
-    const a = document.createElement('a');
-    a.setAttribute('href', link.href);
-    a.textContent = link.text;
-    topMenuEl.appendChild(a);
-  });
+    menuLinks.forEach(function (link) {
+        const a = document.createElement('a');
+        a.setAttribute('href', link.href);
+        a.textContent = link.text;
+        topMenuEl.appendChild(a);
+    });
 });
 
